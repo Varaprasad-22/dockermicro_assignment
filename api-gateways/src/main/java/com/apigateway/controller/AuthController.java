ec7e2b3600b31a376862c6936c709073eecd5832
infinity-1001 <gruitdso555@gmail.com>
2025-12-08 00:18:49 +0530

    added the auth for login and signup

--- api-gateways/src/main/java/com/apigateway/controller/AuthController.java
+++ api-gateways/src/main/java/com/apigateway/controller/AuthController.java
@@ -1,5 +1,25 @@
 package com.apigateway.controller;
 
-public class AuthController {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.apigateway.util.JwtUtil;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final JwtUtil jwtUtil;
+    private final String cookieName;
+    private final long expireSeconds;
+    public AuthController(JwtUtil jwtUtil,
+            @Value("${jwt.cookieName}") String cookieName,
+            @Value("${jwt.expireSeconds}") long expireSeconds) {
+this.jwtUtil = jwtUtil;
+this.cookieName = cookieName;
+this.expireSeconds = expireSeconds;
+}
+    
+    
+    
 }
